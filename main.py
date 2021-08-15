1bc7d0165e400ece7b125b78c57911f3712be4ce
Sion F <github@sions.org>
2021-08-15 08:38:40 -0600

    Copies Daily Drive to Daily Listen playlist

---
+++ main.py
@@ -0,0 +1,54 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+with open("./.secrets") as f:
+    CLIENT_SECRET = f.read().splitlines()[0]
+
+SCOPE = "playlist-modify-public,playlist-modify-private,playlist-read-private,user-library-read,user-read-playback-position"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="91ed165161494fffae34d89d02619204",
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri="http://localhost/callback",
+                                               scope=SCOPE))
+
+def get_playlist(field, search):
+    '''Search through all of the playlists, and return the first uri that matches by field'''
+    playlists = sp.current_user_playlists()
+    while playlists:
+        for playlist in playlists['items']:
+            if playlist[field] == search:
+                return playlist['uri']
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+    return None
+
+DailyDriveID = get_playlist('name', 'Daily Drive')
+
+# Create playlist
+if not get_playlist('name', 'Daily Listen'):
+    sp.user_playlist_create(sp.me()['id'],'Daily Listen',public=False, description='Playlist for the day')
+
+
+DailyListenID = get_playlist('name', 'Daily Listen')
+
+DailyListen = sp.playlist_items(DailyListenID, fields='items(track(uri)),next')
+while DailyListen:
+    itemsDailyListen = [ trackDailyListen['track']['uri'] for trackDailyListen in DailyListen['items'] ]
+    sp.playlist_remove_all_occurrences_of_items(DailyListenID, items=itemsDailyListen)
+    if DailyListen['next']:
+        DailyListen = sp.next(DailyListen)
+    else:
+        DailyListen = None
+
+# Copy playlist contents to target
+fields='items(track(uri)),next' # Next permits pagination
+DailyDrive = sp.playlist_items(DailyDriveID, fields=fields)
+while DailyDrive:
+    itemsDailyDrive = [ trackDailyDrive['track']['uri'] for trackDailyDrive in DailyDrive['items'] ]
+    sp.playlist_add_items(DailyListenID, items=itemsDailyDrive)
+    if DailyDrive['next']:
+        DailyDrive = sp.next(DailyDrive)
+    else:
+        DailyDrive = None
