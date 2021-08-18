1c64f42f41431238ba15c4051d9bf39db029d292
Sion F <github@sions.org>
2021-08-18 06:50:54 -0600

    sorting and filtering is local

--- main.py
+++ main.py
@@ -38,8 +38,6 @@ def cullBlacklist(items, filterList):
     return items
 
 
-DailyDriveID = get_playlist('name', 'Daily Drive')
-
 # Create playlist
 if not get_playlist('name', 'Daily Listen'):
     sp.user_playlist_create(sp.me()['id'],'Daily Listen',public=False, description='Playlist for the day')
@@ -56,21 +54,14 @@ while DailyListen:
     else:
         DailyListen = None
 
-# Copy playlist contents to target
+
+DailyDriveID = get_playlist('name', 'Daily Drive')
+
 fields='items(track(uri)),next' # Next permits pagination
-DailyDrive = sp.playlist_items(DailyDriveID, fields=fields)
-while DailyDrive:
-    itemsDailyDrive = [ trackDailyDrive['track']['uri'] for trackDailyDrive in DailyDrive['items'] ]
+newPlaylist = [ playlistItem['track']['uri'] for playlistItem in sp.playlist_items(DailyDriveID, fields=fields)['items'] ]
 
-    # Cull out blacklisted shows
-    filterDailyDrive = cullBlacklist(itemsDailyDrive, filter_show)
-
-
-    sp.playlist_add_items(DailyListenID, items=itemsDailyDrive)
-    if DailyDrive['next']:
-        DailyDrive = sp.next(DailyDrive)
-    else:
-        DailyDrive = None
+# Cull out blacklisted shows
+newPlaylist = cullBlacklist(newPlaylist, filter_show)
 
 # Get URI list of shows being followed
 
@@ -113,11 +104,28 @@ while savedShows:
 allEpisodes = sorted(allEpisodes, key=lambda episode: episode['release_date'], reverse=True)
 
 # Get count of songs, we want half of that for podcasts
-totalDailyDrive = len(itemsDailyDrive)
 songCount = 0
-for x in itemsDailyDrive:
+for x in newPlaylist:
     if x.split(':')[1] == "track":
         songCount = songCount + 1
 
 singleEpisodes = [ episode['uri'] for episode in allEpisodes[:round(songCount/2)] ]
-sp.playlist_add_items(DailyListenID, items=singleEpisodes)
+newPlaylist.extend(singleEpisodes)
+print(newPlaylist)
+
+# sampleData = ['spotify:track:3DKmwMaqRTOW5Ba5dHijgX', 'spotify:episode:19dyBsqL2gX74pFXK4RSNb', 'spotify:track:4yVJTuaJ4NlCaCUYe9ElIz', 'spotify:track:71hhuJLNpxBQLSbbQ6ZWr4', 'spotify:track:7j31rVgGX9Q2blT92VBEA0', 'spotify:track:7uSsHbBFFAnkRQR1rDwP3L', 'spotify:episode:1wF3w9zP9jlY1sdPbu2ala', 'spotify:track:1N3bdxmsu3wimLy3E4d4cf', 'spotify:track:707mVFEVPIpTWaejXjI7TY', 'spotify:track:5qNEPKqPOECw50RpOGT9ne', 'spotify:track:4DijrJqz7WYjnjHmvdlAyB', 'spotify:track:50jXnyrzW4mcBBqFTMEoAC', 'spotify:track:4wD6EgZCok8Qb5Fs8jszYc', 'spotify:track:6qZjm61s6u8Ead9sWxCDro', 'spotify:track:6QEvc1Xriqvn70EG2VrDy3', 'spotify:track:3SUNHWszXHmCx0J668tqq1', 'spotify:track:11cjKlgVaUvbBjEAAGv9MZ', 'spotify:track:0d28khcov6AiegSCpG5TuT', 'spotify:track:2zzLRQ78kKfPTx8FJQCdC2', 'spotify:track:4WQs5UIokNcsxWumMEfDt3', 'spotify:track:3p0rvHL2zfHAlXAgnHC4GI', 'spotify:track:72hSmnleYTiiOo23q8ZJIS', 'spotify:track:5AKYyNPYhumqKeOMhdEgQO', 'spotify:track:5ylAFXgB62LXZBxUy6cmYg', 'spotify:track:7vA9zWg6fmjLZn105Uj4TE', 'spotify:track:4jFLw7QqWlv3lZr980HyYW', 'spotify:track:03wKMRNYVvw6s9nm4I4jUS', 'spotify:track:7tCHpjktA50ihtkLz6bAnn', 'spotify:track:20I8RduZC2PWMWTDCZuuAN', 'spotify:track:1MzAV3ZqGVU2cmmwmJwymM', 'spotify:track:6E69aBnq0hr6cTGBo39cWD', 'spotify:episode:4Hkx7uiqlmrR4oPzpp2rsk', 'spotify:episode:5oxZSKaJjyjgo10bj7M8Xk', 'spotify:episode:6xYAXwz6oDPE6ApMt1HzV5', 'spotify:episode:5OO67nxhbmCS08l9IDt3It', 'spotify:episode:2yRvXnC1JQzGTmq9kqpCnH', 'spotify:episode:2om6UMZbU7PjLCUFQljNv6', 'spotify:episode:0WlyKekrux44Lxj1Mq2m9u', 'spotify:episode:0txUTbvgXbC57WpgzDXr0z', 'spotify:episode:6rqWdosPJz53cQRzeQ9nfF', 'spotify:episode:4HBdWIIQtrAz10G20esHaY', 'spotify:episode:783lK8XOohpbxoAIpOJyhk', 'spotify:episode:3NdbtyXttT6yJrvsFNIbG0', 'spotify:episode:6oEqn3yJ4I1VNssUGlbWgW', 'spotify:episode:1rPkyn605umnRfJMEbHhsd']
+
+tracks = [ track for track in newPlaylist if track.split(':')[1] == 'track']
+episodes = [episode for episode in newPlaylist if episode.split(':')[1] == 'episode']
+
+sortedPlaylist = list()
+
+while len(tracks) > 0:
+    sortedPlaylist.append(episodes.pop(0))
+    sortedPlaylist.append(tracks.pop(0))
+    try:
+        sortedPlaylist.append(tracks.pop(0))
+    except:
+        pass
+
+sp.playlist_add_items(DailyListenID, items=sortedPlaylist)
